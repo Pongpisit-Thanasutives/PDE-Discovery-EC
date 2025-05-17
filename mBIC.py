53d0e354aede3dc27b6a6c41c4eb39126cf85766
Pongpisit Thanasutives <pongpisit.tha@gmail.com>
2025-05-17 14:45:16 +0900

    Update

--- mBIC.py
+++ mBIC.py
@@ -1,9 +1,15 @@
 import math
+import statsmodels.api as sm
 
 def bic(loglik, k, n):
     if not (n > 0 and k >= 0):
         raise ValueError("Invalid input: n must be > 0 and k >= 0")
     return -2 * loglik + k * math.log(n)
+
+def BIC(X, y):
+    model = sm.OLS(y, X)
+    result = model.fit()
+    return result.bic
 
 def mbic(loglik, k, n, p, const=4):
     '''
@@ -25,6 +31,14 @@ def mbic(loglik, k, n, p, const=4):
     if not (n > 0 and k >= 0 and p > 0 and p/const > 1 and p/k >= 1):
         raise ValueError("Invalid input: ensure n > 0, k >= 0, p > 0, p/const > 1, and p/k >= 1")
     return bic(loglik, k, n) + 2 * k * math.log(p / const - 1)
+
+def MBIC(X, y, num_all_vars=None, const=4)
+    model = sm.OLS(y, X)
+    result = model.fit()
+    loglik = model.loglike(result.params)
+    if num_all_vars is None:
+        num_all_vars = len(result.params)
+    return mbic(loglik, len(result.params), len(y), num_all_vars, const)
 
 def mbic2(loglik, k, n, p, const=4):
     '''
@@ -56,6 +70,14 @@ def mbic2(loglik, k, n, p, const=4):
 
     return mbic(loglik, k, n, p, const) - penalty
 
+def MBIC2(X, y, num_all_vars=None, const=4)
+    model = sm.OLS(y, X)
+    result = model.fit()
+    loglik = model.loglike(result.params)
+    if num_all_vars is None:
+        num_all_vars = len(result.params)
+    return mbic(loglik, len(result.params), len(y), num_all_vars, const)
+
 def ebic(loglik, k, n, p, const=0):
     '''
     loglik	
@@ -74,4 +96,12 @@ def ebic(loglik, k, n, p, const=0):
     A numeric > 0, [0, 1] (0 means uniform prior)
     '''
     return bic(loglik, k, n) + 2 * (1-const) * math.log(math.comb(p, k))
+
+def EBIC(X, y, num_all_vars=None, const=0)
+    model = sm.OLS(y, X)
+    result = model.fit()
+    loglik = model.loglike(result.params)
+    if num_all_vars is None:
+        num_all_vars = len(result.params)
+    return ebic(loglik, len(result.params), len(y), num_all_vars, const)
 
