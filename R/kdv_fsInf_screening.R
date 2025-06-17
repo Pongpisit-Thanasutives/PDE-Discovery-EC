5d819d4e41f08c685854f09cce7a1daf8471b9fb
Pongpisit Thanasutives <pongpisit.tha@gmail.com>
2025-06-17 18:43:10 +0900

    Update

--- R/kdv_fsInf_screening.R
+++ R/kdv_fsInf_screening.R
@@ -6,8 +6,8 @@ np <- import("numpy")
 
 library(selectiveInference)
 
-X_pre <- np$load("../Cache/X_pre_burgers_noise50.npy")
-y_pre <- np$load("../Cache/y_pre_burgers_noise50.npy")
+X_pre <- np$load("../Cache/X_pre_kdv_noise50.npy")
+y_pre <- np$load("../Cache/y_pre_kdv_noise50.npy")
 n = nrow(y_pre)
 
 alpha = 0.05
@@ -16,6 +16,6 @@ fsfit = fs(X_pre,y_pre,intercept=FALSE,normalize=TRUE)
 # run sequential inference with estimated sigma
 out_fs = fsInf(fsfit,alpha=alpha,type="active")
 
-save_file = "./R_data/fsInf_screening_burgers_noise50.rds"
+save_file = "./R_data/fsInf_screening_kdv_noise50.rds"
 saveRDS(out_fs, file = save_file)
 readRDS(save_file)
