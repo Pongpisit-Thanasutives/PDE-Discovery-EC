dff2cc441fe5f109cdf59c280bb4e1fd12dda50b
Pongpisit Thanasutives <pongpisit.tha@gmail.com>
2025-06-18 14:03:41 +0900

    Update

---
+++ R/ks_fsInf_screening.R
@@ -0,0 +1,21 @@
+gc()
+rm(list = ls())
+
+library(reticulate)
+np <- import("numpy")
+
+library(selectiveInference)
+
+X_pre <- np$load("../Cache/X_pre_ks_noise50.npy")
+y_pre <- np$load("../Cache/y_pre_ks_noise50.npy")
+n = nrow(y_pre)
+
+alpha = 0.05
+# run forward stepwise
+fsfit = fs(X_pre,y_pre,intercept=FALSE,normalize=TRUE)
+# run sequential inference with estimated sigma
+out_fs = fsInf(fsfit,alpha=alpha,type="active")
+
+save_file = "./R_data/fsInf_screening_ks_noise50.rds"
+saveRDS(out_fs, file = save_file)
+readRDS(save_file)
