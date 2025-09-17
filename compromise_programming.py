a4720a6b25ca3672bb748d5bdae38c2c3e838479
Pongpisit Thanasutives <pongpisit.tha@gmail.com>
2025-09-17 23:48:41 +0700

    Update

--- compromise_programming.py
+++ compromise_programming.py
@@ -1,6 +1,8 @@
 import numpy as np
 import statsmodels.api as sm
 from collections import Counter
+from bayesian_model_evidence import log_evidence
+# MCDM/MCDA
 from pymcdm import weights as obj_w
 from pymcdm.methods import TOPSIS, MABAC, COMET, SPOTIS
 from pymcdm.methods.comet_tools import MethodExpert
@@ -11,35 +13,65 @@ def compromise_programming(
     best_subsets,
     dataset,
     weight="entropy_weights",
+    criterion="bic",
     ssr_normalization=None,
-    bic_normalization=None,
+    ic_normalization=None,
+    ic_kwargs={},
+    verbose=True
 ):
+    assert criterion in {"bic", "bme"}
     XX, yy = dataset
 
     ssr = []
-    bic = []
+    ic = []
     complexity = []
     for efi in best_subsets:
+        # SSR and Complexity
         ols_result = sm.OLS(yy, XX[:, efi]).fit()
         ssr.append(ols_result.ssr)
-        bic.append(ols_result.bic)
         complexity.append(len(ols_result.params))
-    bic = np.array(bic)
+        # Criterion: BIC or Bayesian Model Evidence (BME)
+        if criterion == "bme":
+            ic.append(-log_evidence(XX[:, efi], yy, **ic_kwargs))
+        else:
+            ic.append(ols_result.bic)
+    ic = np.array(ic)
 
     F = np.stack((ssr, complexity), axis=1)
+    n_alternatives, n_criteria = F.shape
+    types = np.array([-1 for _ in range(n_criteria)]) # default to minimization
     if ssr_normalization is not None:
-        F[:, 0:1] = getattr(normalizations, ssr_normalization)(F[:, 0:1])
-    obj_weights = getattr(obj_w, weight)(F, types=np.array([-1, -1]))
+        if np.any(F[:, 0:1] < 0):
+            F[:, 0:1] = -getattr(normalizations, ssr_normalization)(-F[:, 0:1])
+        else:
+            F[:, 0:1] = getattr(normalizations, ssr_normalization)(F[:, 0:1])
+    obj_weights = getattr(obj_w, weight)(F, types=np.array(types))
 
-    F[:, 0:1] = -bic.reshape(-1, 1)
-    if bic_normalization is not None:
-        F[:, 0:1] = getattr(normalizations, bic_normalization)(F[:, 0:1])
+    filtered_F = F.copy()
+    filtered_F[:, 0:1] = ic.reshape(-1, 1)
+    if ic_normalization is not None:
+        if np.any(filtered_F[:, 0:1] < 0):
+            filtered_F[:, 0:1] = -getattr(normalizations, ic_normalization)(-filtered_F[:, 0:1])
+        else:
+            filtered_F[:, 0:1] = getattr(normalizations, ic_normalization)(filtered_F[:, 0:1])
 
-    types = np.array([+1, -1])
-    ranks, prefs = mcdm(F, obj_weights, types)
-    balance_point, most_common = ranks2decision(ranks)
+    decision_seq = []
+    preference_seq = []
+    while len(filtered_F) > 2:
+        ranks, prefs = mcdm(filtered_F, obj_weights, types)
+        balance_point, most_common = ranks2decision(ranks)
+        if verbose:
+            print(filtered_F, most_common)
 
-    return F[balance_point], most_common, ranks, prefs
+        filtered_F = filtered_F[:balance_point+1]
+
+        decision_seq.append(most_common)
+        preference_seq.append(prefs)
+
+        if len(most_common) == 1:
+            break
+
+    return F, decision_seq, preference_seq
 
 
 def mcdm(F, obj_weights, types):
@@ -54,14 +86,16 @@ def mcdm(F, obj_weights, types):
     prefs = []
     ranks = []
     for method in methods:
-        pref = method(F, obj_weights, types)
+        pref = method(F, obj_weights, types) # .tolist()
+        rank = method.rank(pref) # .tolist()
         prefs.append(pref)
-        ranks.append(method.rank(pref))
+        ranks.append(rank)
 
     return ranks, prefs
 
 
 def ranks2decision(ranks):
     most_common = Counter(np.argmin(ranks, axis=1)).most_common()
+    most_common = sorted(most_common, key=lambda _: (_[1], _[0]), reverse=True)
     balance_point = most_common[0][0]
     return balance_point, most_common
